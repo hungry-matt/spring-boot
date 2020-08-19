4caa70545b927cc2e4e324260ec4963515b04ecc
hungry-matt <yoodjin2@gmail.com>
2020-08-19 23:49:30 +0900

    add: 게시글 수정 기능

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -10,23 +10,44 @@ var index = {
             title : $('#title').val()
             , author : $('#author').val()
             , content : $('#content').val()
+            , message : "글이 등록 되었습니다."
+            , url : '/api/v1/posts'
+            , type : 'post'
         };
 
         if(!confirm("저장 하시겠습니까?")) return false;
 
+        this.postAjax(data);
+    }
+    , update : function() {
+        var id = $('#id').val();
+
+        var date = {
+            title: $('#title').val()
+            , content : $('#content').val()
+            , message : "글이 수정 되었습니다."
+            , url : '/api/v1/posts/' + id
+            , type : 'put'
+        };
+
+        this.postAjax(data);
+    }
+    , postAjax : function(data) {
         $.ajax({
-            type : 'post'
-            , url : '/api/v1/posts'
+            type : data.type
+            , url : data.url
             , dataType : 'json'
             , contentType : 'application/json; charset=utf-8'
             , data : JSON.stringify(data)
         }).done(function(){
-            alert('글이 등록되었습니다.');
+            alert(data.message);
             window.location.href = '/';
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
     }
 };
+
+
 
 index.init();
