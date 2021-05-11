bec70590be13a76e06952813de77ac9c61717846
matt <yoodjin2@gmail.com>
2021-05-11 23:47:43 +0900

    change: 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,13 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl spring-boot | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid: ${CURRENT_PID}"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -23,6 +23,8 @@ fi
   echo "> 새 애플리케이션 배포"
 
   JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+  echo "> JAR_NAME: $JAR_NAME"
 
   echo "> $JAR_NAME 실행 권한 추가"
 
